64ef49f3eb5ed2144ccd565e6af59bb94630f6e4
Enol García Pérez <99491388+Enolgp@users.noreply.github.com>
2024-06-14 23:56:42 +0200

    Add db via upload

---
+++ db.js
@@ -0,0 +1,60 @@
+{
+    "element"[
+        {
+            "id": 1,
+            "data": "Alguien hace la broma con la palabra fin y el nombre del capitán"
+        },
+        {
+            "id": 2,
+            "data": "Sobra comida"
+        },
+        {
+            "id": 3,
+            "data": "Brigit menciona 'anillo' o 'hablar com los animales'"
+        },
+        {
+            "id": 4,
+            "data": "Finlark hace una mala tirada"
+        },
+        {
+            "id": 5,
+            "data": "Un speech del Master es interrumpido por su madre"
+        },
+        {
+            "id": 6,
+            "data": "Sjach hace uwu"
+        },
+        {
+            "id": 7,
+            "data": "LEOG intenta regatear un precio"
+        },
+        {
+            "id": 8,
+            "data": "Shamash se desespera por culpa de la tripulación"
+        },
+        {
+            "id": 9,
+            "data": "Nova hechiza a alguien"
+        },
+        {
+            "id": 10,
+            "data": "Hay un nuevo integrante en la tripulación o al menos alguien ha sido invitado"
+        },
+        {
+            "id": 11,
+            "data": "Los oficiales cometen un delito"
+        },
+        {
+            "id": 12,
+            "data": "Destello muestra odio contra alguno de los oficiales"
+        },
+        {
+            "id": 13,
+            "data": "Nando se escaquea del trabajo (o al menos lo intenta)"
+        },
+        {
+            "id": 14,
+            "data": "Sjach entrena con alguien o le pide que entrene con él"
+        }
+    ]
+}
